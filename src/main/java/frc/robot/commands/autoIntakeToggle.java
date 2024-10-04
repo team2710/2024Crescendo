83cf7232bcacdbb278b66225653d2414e8722c74
EV3KevinDEV <kevinholding92@gmail.com>
2024-10-03 20:55:39 -0700

    add timer

--- src/main/java/frc/robot/commands/autoIntakeToggle.java
+++ src/main/java/frc/robot/commands/autoIntakeToggle.java
@@ -4,32 +4,29 @@ import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.Vector;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.EndEffector;
 import frc.robot.subsystems.Pivot;
-import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.Vector;
-import edu.wpi.first.wpilibj.Timer;
 
 public class autoIntakeToggle extends Command {
-    EndEffector m_intake;
-    private double m_timeout;
+    private EndEffector m_intake;
     private Timer m_timer;
+    private static final double TIMEOUT_SECONDS = 1.0;
 
     public autoIntakeToggle(EndEffector m_intake) {
         this.m_intake = m_intake;
- 
-
+        m_timer = new Timer();
         // Use addRequirements() here to declare subsystem dependencies.
-        // Configure additional PID options by calling `getController` here
         addRequirements(m_intake);
     }
 
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-
+        m_timer.reset();
+        m_timer.start();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
@@ -42,12 +39,12 @@ public class autoIntakeToggle extends Command {
     @Override
     public void end(boolean interrupted) {
         m_intake.stopIntake();
+        m_timer.stop();
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return m_intake.isNoteDetected();
+        return m_intake.isNoteDetected() || m_timer.hasElapsed(TIMEOUT_SECONDS);
     }
-
 }
